4a55386dbbf0c3a98c498ee25eb2634b091a81fd
Rayan Robion <rayan.robion7@gmail.com>
2023-04-04 18:23:58 +0200

    add mason / mason-lspconfig

--- lua/lspconfig-setup.lua
+++ lua/lspconfig-setup.lua
@@ -4,24 +4,6 @@ if not status then
     return
 end
 
-local on_attach = function(client, bufnr)
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-    local vim_set_normalmode_keymap = function(keymap, action)
-        vim.keymap.set('n', keymap, action, bufopts)
-    end
-
-    -- enable completion triggered by <c-x><c-o> | <S-Tab>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    vim_set_normalmode_keymap('gD', vim.lsp.buf.declaration)
-    vim_set_normalmode_keymap('gd', vim.lsp.buf.definition)
-    vim_set_normalmode_keymap('K', vim.lsp.buf.hover)
-    vim_set_normalmode_keymap('gi', vim.lsp.buf.implementation)
-    vim_set_normalmode_keymap('<C-k>', vim.lsp.buf.signature_help)
-    vim_set_normalmode_keymap('gl', vim.diagnostic.open_float)
-end
-
 local diagnostic_config = {
     virtual_text = false,
     update_in_insert = false,
@@ -46,47 +28,23 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 })
 vim.diagnostic.config(diagnostic_config)
 
-local lsp_flags = {
-    debounce_text_changes = 150,
-}
 
-local lsp_servers = {
-    {
-        name = 'lua_ls',
-        cmd = { "lua-language-server.sh" },
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = { 'vim' }
-                }
-            }
-        }
-    },
-    {
-        name = 'rust_analyzer',
-        settings = { ["rust-analyzer"] = {} }
-    },
-    {
-        name = 'solargraph',
-        cmd = { "solargraph", "stdio" },
-        root_dir = function()
-            return vim.loop.cwd()
-        end
-    },
-}
-
-for _,lsp_server in pairs(lsp_servers) do
-    ---@type table<string, string|table|function>
-	local lsp_server_setup = {
-		on_attach = on_attach,
-		flags = lsp_flags,
-	}
-
-	for k,v in pairs(lsp_server) do
-	    if lsp_server.name ~= v then
-            lsp_server_setup[k] = v
-        end
-	end
-
-    nvim_lsp[lsp_server.name].setup(lsp_server_setup)
-end
+-- local lsp_servers = {
+--     -- install LSP servers with :MasonInstall <LSP_server>
+-- }
+-- 
+-- for _,lsp_server in pairs(lsp_servers) do
+--     ---@type table<string, string|table|function>
+-- 	local lsp_server_setup = {
+-- 		on_attach = on_attach,
+-- 		flags = lsp_flags,
+-- 	}
+-- 
+-- 	for k,v in pairs(lsp_server) do
+-- 	    if lsp_server.name ~= v then
+--             lsp_server_setup[k] = v
+--         end
+-- 	end
+-- 
+--     nvim_lsp[lsp_server.name].setup(lsp_server_setup)
+-- end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,31 @@
 return {
+    -- Colorschemes
+    "phha/zenburn.nvim",
+    "rbnrayan/yello_world.nvim",
+
     {
         'glepnir/dashboard-nvim',
         event = 'VimEnter',
         dependencies = { {'nvim-tree/nvim-web-devicons'} }
     },
-    "neovim/nvim-lspconfig",
-    "phha/zenburn.nvim",
-    "rbnrayan/yello_world.nvim",
+
+    -- Telescope
     "nvim-treesitter/nvim-treesitter",
     {
         "nvim-telescope/telescope.nvim", tag = "0.1.1",
         dependencies = { "nvim-lua/plenary.nvim" },
         lazy = false,
     },
+
+    -- LSP
+    "neovim/nvim-lspconfig",
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false
+    },
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+    }
 }
 
