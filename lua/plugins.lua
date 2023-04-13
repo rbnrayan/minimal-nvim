ae81c6513bfe519c933117baae2f1001b350efb9
Rayan Robion <rayan.robion7@gmail.com>
2023-04-13 21:47:56 +0200

    clean plugins.lua file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,8 @@
 return {
     -- Colorschemes
-    -- "phha/zenburn.nvim",
     "rbnrayan/yello_world.nvim",
 
-    -- {
-    --     'glepnir/dashboard-nvim',
-    --     event = 'VimEnter',
-    --     dependencies = { {'nvim-tree/nvim-web-devicons'} }
-    -- },
-
     "nvim-treesitter/nvim-treesitter",
-    -- Telescope
-    -- {
-    --     "nvim-telescope/telescope.nvim", tag = "0.1.1",
-    --     dependencies = { "nvim-lua/plenary.nvim" },
-    --     cmd = "TSUpdate",
-    --     -- lazy = false,
-    -- },
 
     -- LSP
     "neovim/nvim-lspconfig",
